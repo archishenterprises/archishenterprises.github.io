a3f3e24ded946b5fb47c4611bf1b307554cf0299
archishenterprises <archishenterprises@users.noreply.github.com>
2021-11-27 20:01:33 +0000

    deployed via Gatsby Publish Action 🎩 for b6bf64dc5c2bcafd2b6dc9983c1af48f48482f6f

---
+++ component---node-modules-reflexjs-gatsby-theme-base-src-pages-404-js-ce3c2866230830138fd1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"8snC":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),r=t("zCiS"),o=t("Jg8R");n.default=function(){return a.createElement(r.Container,{textAlign:"center"},a.createElement(r.H1,null,"404 - Page not found"),a.createElement(o.b,{to:"/"},a.createElement(o.a,{name:"arrow-left",mr:"2"}),"Back home"))}}}]);
+//# sourceMappingURL=component---node-modules-reflexjs-gatsby-theme-base-src-pages-404-js-ce3c2866230830138fd1.js.map
